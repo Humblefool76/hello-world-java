d749d77a46c71352a833ece4838add3483d3f82d
Humblefool76 <93398938+Humblefool76@users.noreply.github.com>
2022-09-03 14:46:59 +0530

    Update HelloWorld.java
    commit the change

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,14 @@
-public class HelloWorld {
+public class calculator {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int a =10;
+    int b = 5;
+    
+    System.out.println("Addition : " + (a+b));
+    
+    System.out.println("Subtraction : " + (a-b));
+    
+    System.out.println("Multiplication : " + (a*b));
+    
+    System.out.println("Division : " + (a/b));
   }
 }
